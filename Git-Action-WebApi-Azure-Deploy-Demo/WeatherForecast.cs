9f31b59964c486b8700e889a0b62da8258a25141
amish.khatri <amish.khatri@gmail.com>
2022-03-05 08:01:55 +0530

    Add project files.

---
+++ Git-Action-WebApi-Azure-Deploy-Demo/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Git_Action_WebApi_Azure_Deploy_Demo
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
